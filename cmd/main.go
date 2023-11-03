5b8eb8b16b1cd45a950d3b8b152411466af63923
Pakawat Bamrungkit <pakawatbamrungkit@Pakawats-Mac-Studio.local>
2023-11-03 13:50:21 +0700

    commit

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"context"
-	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
-	"net/url"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -18,35 +15,20 @@ import (
 	_notificationService "github.com/gamepkw/notification-banking-microservice/internal/services"
 )
 
+func init() {
+	viper.SetConfigFile(`config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if viper.GetBool(`debug`) {
+		log.Println("Service RUN on DEBUG mode")
+	}
+}
+
 func main() {
 	// logger.Info("start program...")
-
-	dbHost := viper.GetString(`database.host`)
-	dbPort := viper.GetString(`database.port`)
-	dbUser := viper.GetString(`database.user`)
-	dbPass := viper.GetString(`database.pass`)
-	dbName := viper.GetString(`database.name`)
-	dbconnection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
-	val := url.Values{}
-	val.Add("parseTime", "true")
-	val.Add("loc", "Asia/Bangkok")
-	dsn := fmt.Sprintf("%s?%s", dbconnection, val.Encode())
-	dbConn, err := sql.Open(`mysql`, dsn)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = dbConn.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer func() {
-		err := dbConn.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
 
 	config := sarama.NewConfig()
 	config.ClientID = "my-kafka-client"
